81286f3e70f3103f0b60b304aef7f1d5485b310f
Abdelatif H <ab.hassaine@protonmail.com>
2023-11-12 14:44:38 +0100

    fix/Fix responsive on nominations section

--- template-parts/nomination.php
+++ template-parts/nomination.php
@@ -1,14 +1,3 @@
-<!--<section id="nomination" class="nomination">-->
-<!--    <div class="container">-->
-<!--        <div class="nomination__title">-->
-<!--            <h3>Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>-->
-<!--        </div>-->
-<!--        <div class="nomination__img">-->
-<!--            <img src="--><?php //echo get_stylesheet_directory_uri() . '/assets/media/images/nomination-oscars-2021.png'; ?><!--" alt="nomination aux oscars 2021">-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</section>-->
-
 <section id="nomination" class="nomination">
     <article>
         <p>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</p>
